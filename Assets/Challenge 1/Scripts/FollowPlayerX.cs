532daf7f87900685b6dcddbc509abb1ee23fd53d
Thomaspls-unity <152604731+Thomaspls-unity@users.noreply.github.com>
2023-12-08 15:53:43 +0000

    Challenge 2.5 Complete
    -Made the plane automatically go forward
    -Adjusted the speed so that it's going a reasonable speed
    -No longer tilting automatically and only when the user input of up/down is pressed
    -Camera is to the side of the plane and following along smoothly
    -Made the propeller spin too :)

--- Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -4,18 +4,33 @@ using UnityEngine;
 
 public class FollowPlayerX : MonoBehaviour
 {
-    public GameObject plane;
+    public Transform plane;
     private Vector3 offset;
+    public float smoothSpeed = 0.1f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        offset = transform.position - plane.position;
     }
 
     // Update is called once per frame
-    void Update()
+    private void LateUpdate()
+    {
+        MoveCamWithLerp();
+    }
+
+    private void MoveCamWithoutLerp()
     {
         transform.position = plane.transform.position + offset;
+    }
+
+    private void MoveCamWithLerp()
+    {
+        Vector3 expectedPosition = plane.position + offset;
+
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, expectedPosition, smoothSpeed);
+
+        transform.position = smoothPosition;
     }
 }
--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -6,7 +6,9 @@ public class PlayerControllerX : MonoBehaviour
 {
     public float speed;
     public float rotationSpeed;
+    public float propellerSpeed;
     public float verticalInput;
+    public float horizontalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,15 +17,28 @@ public class PlayerControllerX : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        // get the user's vertical input
-        verticalInput = Input.GetAxis("Vertical");
+        MovePlaneForwards();
+        TiltPlane();
+        SpinPropeller();
+    }
 
+    private void MovePlaneForwards()
+    {
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.forward * speed);
+        transform.Translate(Vector3.forward * speed * Time.fixedDeltaTime);
+    }
 
+    private void TiltPlane()
+    {
+        verticalInput = Input.GetAxis("Vertical");
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * verticalInput * rotationSpeed * Time.fixedDeltaTime);
+    }
+
+    private void SpinPropeller()
+    {
+        transform.Rotate(Vector3.back * propellerSpeed * Time.fixedDeltaTime);
     }
 }
